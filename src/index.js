125f8815fa3725561856e14a9da95ca6ab1c0c0b
rhkt <git190811@gmail.com>
2021-09-09 08:21:44 +0900

    ファーストコミット

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Button, Divider } from "@material-ui/core";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Game extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>ハローワールド</h1>
+				<Button size="small" variant="contained" color="primary">
+					全てを見る
+				</Button>
+			</div>
+		);
+	}
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Game />, document.getElementById("root"));
