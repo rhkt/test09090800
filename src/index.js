9be8e4aa842b144372e5bd2d8e307622154f460d
rhkt <git190811@gmail.com>
2021-09-09 19:37:47 +0900

    2回目のコミット

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ class Game extends React.Component {
 		return (
 			<div>
 				<h1>ハローワールド</h1>
+				<h2>ハローワールド</h2>
 				<Button size="small" variant="contained" color="primary">
 					全てを見る
 				</Button>
